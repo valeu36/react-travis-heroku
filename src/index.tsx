b47e0f7558715d8e99505c1d1bb011c53e8b65a1
Andrey Dihtiariov <andrey.d@giantleaplab.com>
2020-08-22 01:44:32 +0300

    init commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Listings } from './sections'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Listings />
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
