2ab02077be914d3c08eaef164d853cb76c73f083
Andrey Dihtiariov <andrey.d@giantleaplab.com>
2020-08-22 02:27:11 +0300

    listing folder deleted

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Listings } from './sections'
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Listings />
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 )
--- src/sections/Listings/Listings.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export const Listings = () => {
-  return <h2>TinyHouse Listings</h2>
-}
